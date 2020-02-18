c2f964746329665b17bc6f9f507755cc5355a7c3
kevalmorabia97 <kevalmorabia97@gmail.com>
2020-02-18 13:45:55 -0600

    update main.py for 5-Fold CV

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ from utils import print_and_log
 parser = argparse.ArgumentParser('Train Model')
 parser.add_argument('-d', '--device', type=int, default=0)
 parser.add_argument('-e', '--n_epochs', type=int, default=100)
-parser.add_argument('-bb', '--backbone', type=str, default='alexnet', choices=['alexnet', 'resnet'])
+parser.add_argument('-bb', '--backbone', type=str, default='resnet', choices=['alexnet', 'resnet'])
 parser.add_argument('-tc', '--trainable_convnet', type=int, default=1, choices=[0,1])
 parser.add_argument('-lr', '--learning_rate', type=float, default=0.0005)
 parser.add_argument('-bs', '--batch_size', type=int, default=25)
 parser.add_argument('-c', '--context', type=int, default=1, choices=[0,1])
 parser.add_argument('-cs', '--context_size', type=int, default=6)
 parser.add_argument('-att', '--attention', type=int, default=1, choices=[0,1])
-parser.add_argument('-hd', '--hidden_dim', type=int, default=300)
-parser.add_argument('-r', '--roi', type=int, default=1)
+parser.add_argument('-hd', '--hidden_dim', type=int, default=200)
+parser.add_argument('-r', '--roi', type=int, default=3)
 parser.add_argument('-bbf', '--bbox_feat', type=int, default=1, choices=[0,1])
-parser.add_argument('-wd', '--weight_decay', type=float, default=0)
-parser.add_argument('-dp', '--drop_prob', type=float, default=0.5)
-parser.add_argument('-mbb', '--max_bg_boxes', type=int, default=-1)
+parser.add_argument('-wd', '--weight_decay', type=float, default=1e-3)
+parser.add_argument('-dp', '--drop_prob', type=float, default=0.2)
+parser.add_argument('-mbb', '--max_bg_boxes', type=int, default=100)
 parser.add_argument('-nw', '--num_workers', type=int, default=8)
 args = parser.parse_args()
 
@@ -50,21 +50,14 @@ IMG_HEIGHT = 1280 # Image assumed to have same height and width
 EVAL_INTERVAL = 2 # Number of Epochs after which model is evaluated
 NUM_WORKERS = args.num_workers # multithreaded data loading
 
-DATA_DIR = '../data/v3/' # Contains .png and .pkl files for train and test data
-OUTPUT_DIR = 'results_data_v3/attn' # logs are saved here!
+DATA_DIR = '../data/v3/' # Contains imgs/*.png and bboxes/*.pkl files
+SPLIT_DIR = 'splits'
+CV_FOLDS = ['Fold-1', 'Fold-2', 'Fold-3', 'Fold-4', 'Fold-5'] # these dirs are inside `SPLIT_DIR`
+OUTPUT_DIR = 'results_5-Fold_CV' # results are saved here!
 # NOTE: if same hyperparameter configuration is run again, previous log file and saved model will be overwritten
-
-if not os.path.exists(OUTPUT_DIR):
-    os.makedirs(OUTPUT_DIR)
-
-SPLIT_DIR = 'splits_v3'
-train_img_ids = np.loadtxt('%s/train_imgs.txt' % SPLIT_DIR, np.int32)
-val_img_ids = np.loadtxt('%s/val_imgs.txt' % SPLIT_DIR, np.int32)
-test_img_ids = np.loadtxt('%s/test_imgs.txt' % SPLIT_DIR, np.int32)
 
 # for calculating macro accuracy
 webpage_info = np.loadtxt('%s/webpage_info.csv' % SPLIT_DIR, str, delimiter=',', skiprows=1) # (img_id, domain) values
-test_domains = np.loadtxt('%s/test_domains.txt' % SPLIT_DIR, str)
 
 ########## HYPERPARAMETERS ##########
 N_EPOCHS = args.n_epochs
@@ -76,74 +69,76 @@ USE_CONTEXT = bool(args.context)
 CONTEXT_SIZE = args.context_size if USE_CONTEXT else 0
 USE_ATTENTION = bool(args.attention) if USE_CONTEXT else False
 HIDDEN_DIM = args.hidden_dim if USE_CONTEXT and USE_ATTENTION else 0
-ROI_POOL_OUTPUT_SIZE = (args.roi, args.roi)
+ROI_OUTPUT = (args.roi, args.roi)
 USE_BBOX_FEAT = bool(args.bbox_feat)
 WEIGHT_DECAY = args.weight_decay
 DROP_PROB = args.drop_prob
 MAX_BG_BOXES = args.max_bg_boxes if args.max_bg_boxes > 0 else -1
 
-params = '%s lr-%.0e batch-%d c-%d cs-%d att-%d hd-%d roi-%d bbf-%d wd-%.0e dp-%.2f mbb-%d' % (BACKBONE, LEARNING_RATE, BATCH_SIZE, USE_CONTEXT, CONTEXT_SIZE,
-    USE_ATTENTION, HIDDEN_DIM, ROI_POOL_OUTPUT_SIZE[0], USE_BBOX_FEAT, WEIGHT_DECAY, DROP_PROB, MAX_BG_BOXES)
-log_file = '%s/%s logs.txt' % (OUTPUT_DIR, params)
-test_acc_imgwise_file = '%s/%s test_acc_imgwise.csv' % (OUTPUT_DIR, params)
-test_acc_domainwise_file = '%s/%s test_acc_domainwise.csv' % (OUTPUT_DIR, params)
-model_save_file = '%s/%s saved_model.pth' % (OUTPUT_DIR, params)
+params = '%s lr-%.0e batch-%d c-%d cs-%d att-%d hd-%d roi-%d bbf-%d wd-%.0e dp-%.2f mbb-%d' % (BACKBONE, LEARNING_RATE, BATCH_SIZE,
+    USE_CONTEXT, CONTEXT_SIZE, USE_ATTENTION, HIDDEN_DIM, ROI_OUTPUT[0], USE_BBOX_FEAT, WEIGHT_DECAY, DROP_PROB, MAX_BG_BOXES)
+results_dir = '%s/%s' % (OUTPUT_DIR, params)
+fold_wise_acc_file = '%s/fold_wise_acc.csv' % results_dir
 
-print('logs will be saved in \"%s\"' % (log_file))
-print_and_log('Backbone Convnet: %s' % (BACKBONE), log_file, 'w')
-print_and_log('Trainable Convnet: %s' % (TRAINABLE_CONVNET), log_file)
-print_and_log('Learning Rate: %.0e' % (LEARNING_RATE), log_file)
-print_and_log('Batch Size: %d' % (BATCH_SIZE), log_file)
-print_and_log('Context: %s' % (USE_CONTEXT), log_file)
-print_and_log('Context Size: %d' % (CONTEXT_SIZE), log_file)
-print_and_log('Attention: %s' % (USE_ATTENTION), log_file)
-print_and_log('Hidden Dim: %d' % (HIDDEN_DIM), log_file)
-print_and_log('RoI Pool Output Size: (%d, %d)' % ROI_POOL_OUTPUT_SIZE, log_file)
-print_and_log('BBox Features: %s' % (USE_BBOX_FEAT), log_file)
-print_and_log('Weight Decay: %.0e' % (WEIGHT_DECAY), log_file)
-print_and_log('Dropout Probability: %.2f' % (DROP_PROB), log_file)
-print_and_log('Max BG Boxes: %d\n' % (MAX_BG_BOXES), log_file)
+if not os.path.exists(results_dir):
+    os.makedirs(results_dir)
 
-########## DATA LOADERS ##########
-train_loader, val_loader, test_loader = load_data(DATA_DIR, train_img_ids, val_img_ids, test_img_ids, USE_CONTEXT, CONTEXT_SIZE, BATCH_SIZE,
-                                                  NUM_WORKERS, MAX_BG_BOXES)
-
-########## CREATE MODEL & LOSS FN ##########
-model = WebObjExtractionNet(ROI_POOL_OUTPUT_SIZE, IMG_HEIGHT, N_CLASSES, BACKBONE, USE_CONTEXT, USE_ATTENTION, HIDDEN_DIM, TRAINABLE_CONVNET, DROP_PROB,
-                            USE_BBOX_FEAT, CLASS_NAMES).to(device)
-
-optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
 criterion = nn.CrossEntropyLoss(reduction='sum').to(device)
 
-########## TRAIN MODEL ##########
-val_acc_avg = train_model(model, train_loader, optimizer, criterion, N_EPOCHS, device, val_loader, EVAL_INTERVAL, log_file, 'ckpt_%d.pth' % args.device)
+for fold in CV_FOLDS:
+    print('\n>>> Training on %s...' % fold)
+    ########## DATA LOADERS ##########
+    train_img_ids = np.loadtxt('%s/%s/train_imgs.txt' % (SPLIT_DIR, fold), np.int32)
+    val_img_ids = np.loadtxt('%s/%s/val_imgs.txt' % (SPLIT_DIR, fold), np.int32)
+    test_img_ids = np.loadtxt('%s/%s/test_imgs.txt' % (SPLIT_DIR, fold), np.int32)
+    test_domains = np.loadtxt('%s/%s/test_domains.txt' % (SPLIT_DIR, fold), str)
 
-########## EVALUATE TEST PERFORMANCE ##########
-print('Evaluating test data class wise accuracies...')
-img_acc_top1, class_acc_top1 = evaluate_model(model, test_loader, device, 1, 'TEST', log_file)
-_, class_acc_top2 = evaluate_model(model, test_loader, device, 2, 'TEST', log_file)
+    train_loader, val_loader, test_loader = load_data(DATA_DIR, train_img_ids, val_img_ids, test_img_ids, USE_CONTEXT, CONTEXT_SIZE,
+                                                      BATCH_SIZE, NUM_WORKERS, MAX_BG_BOXES)
 
-np.savetxt(test_acc_imgwise_file, img_acc_top1, '%d,%.2f,%.2f,%.2f', ',', header='img_id,price_acc,title_acc,image_acc', comments='')
+    log_file = '%s/%s logs.txt' % (results_dir, fold)
+    test_acc_imgwise_file = '%s/%s test_acc_imgwise.csv' % (results_dir, fold)
+    test_acc_domainwise_file = '%s/%s test_acc_domainwise.csv' % (results_dir, fold)
+    model_save_file = '%s/%s saved_model.pth' % (results_dir, fold)
 
-print('Evaluating per domain accuracy for %d test domains...' % len(test_domains))
-with open (test_acc_domainwise_file, 'w') as f:
-    f.write('Domain,N_examples,%s,%s,%s\n' % (CLASS_NAMES[1], CLASS_NAMES[2], CLASS_NAMES[3]))
-    for domain in test_domains:
-        domain_imgs = webpage_info[np.isin(webpage_info[:,1], domain), 0].astype(np.int32)
-        class_acc = img_acc_top1[np.isin(img_acc_top1[:,0], domain_imgs), 1:].mean(0)*100
-        f.write('%s,%d,%.2f,%.2f,%.2f\n' % (domain, len(domain_imgs), class_acc[0], class_acc[1], class_acc[2]))
+    print('logs will be saved in \"%s\"' % (log_file))
+    print_and_log('Backbone Convnet: %s' % (BACKBONE), log_file, 'w')
+    print_and_log('Trainable Convnet: %s' % (TRAINABLE_CONVNET), log_file)
+    print_and_log('Learning Rate: %.0e' % (LEARNING_RATE), log_file)
+    print_and_log('Batch Size: %d' % (BATCH_SIZE), log_file)
+    print_and_log('Use Context: %s' % (USE_CONTEXT), log_file)
+    print_and_log('Context Size: %d' % (CONTEXT_SIZE), log_file)
+    print_and_log('Use Attention: %s' % (USE_ATTENTION), log_file)
+    print_and_log('Hidden Dim: %d' % (HIDDEN_DIM), log_file)
+    print_and_log('RoI Pool Output Size: (%d, %d)' % ROI_OUTPUT, log_file)
+    print_and_log('BBox Features: %s' % (USE_BBOX_FEAT), log_file)
+    print_and_log('Weight Decay: %.0e' % (WEIGHT_DECAY), log_file)
+    print_and_log('Dropout Probability: %.2f' % (DROP_PROB), log_file)
+    print_and_log('Max BG Boxes: %d\n' % (MAX_BG_BOXES), log_file)
 
-macro_acc_test = np.loadtxt(test_acc_domainwise_file, delimiter=',', skiprows=1, dtype=str)[:,2:].astype(np.float32).mean(0)
-for i in range(1, len(CLASS_NAMES)):
-    print_and_log('%s Macro Acc: %.2f%%' % (CLASS_NAMES[i], macro_acc_test[i-1]), log_file)
+    ########## TRAIN MODEL ##########
+    model = WebObjExtractionNet(ROI_OUTPUT, IMG_HEIGHT, N_CLASSES, BACKBONE, USE_CONTEXT, USE_ATTENTION, HIDDEN_DIM, TRAINABLE_CONVNET,
+                                DROP_PROB, USE_BBOX_FEAT, CLASS_NAMES).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+    val_acc = train_model(model, train_loader, optimizer, criterion, N_EPOCHS, device, val_loader, EVAL_INTERVAL, log_file, 'ckpt_%d.pth' % args.device)
+    torch.save(model.state_dict(), model_save_file)
 
-########## SAVE MODEL ##########
-torch.save(model.state_dict(), model_save_file)
-print_and_log('Model can be restored from \"%s\"' % (model_save_file), log_file)
+    print('Evaluating on test data...')
+    img_acc, class_acc = evaluate_model(model, test_loader, device, 1, 'TEST', log_file)
+    _, class_acc_top2 = evaluate_model(model, test_loader, device, 2, 'TEST', log_file)
 
-with open('%s/comparison.csv' % OUTPUT_DIR, 'a') as f:
-    f.write('%s,%.0e,%d,%d,%d,%d,%d,%d,%d,%.0e,%.2f,%d,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n' % (BACKBONE, LEARNING_RATE, BATCH_SIZE, USE_CONTEXT,
-        CONTEXT_SIZE, USE_ATTENTION, HIDDEN_DIM, ROI_POOL_OUTPUT_SIZE[0], USE_BBOX_FEAT, WEIGHT_DECAY, DROP_PROB, MAX_BG_BOXES, val_acc_avg,
-        class_acc_top1[0], class_acc_top2[0], macro_acc_test[0], class_acc_top1[1], class_acc_top2[1], macro_acc_test[1],
-        class_acc_top1[2], class_acc_top2[2], macro_acc_test[2]))
+    np.savetxt(test_acc_imgwise_file, img_acc, '%d,%.2f,%.2f,%.2f', ',', header='img_id,price_acc,title_acc,image_acc', comments='')
 
+    with open (test_acc_domainwise_file, 'w') as f:
+        f.write('Domain,N_examples,%s,%s,%s\n' % (CLASS_NAMES[1], CLASS_NAMES[2], CLASS_NAMES[3]))
+        for domain in test_domains:
+            domain_imgs = webpage_info[np.isin(webpage_info[:,1], domain), 0].astype(np.int32)
+            class_acc = img_acc[np.isin(img_acc[:,0], domain_imgs), 1:].mean(0)*100
+            f.write('%s,%d,%.2f,%.2f,%.2f\n' % (domain, len(domain_imgs), class_acc[0], class_acc[1], class_acc[2]))
+    macro_acc_test = np.loadtxt(test_acc_domainwise_file, delimiter=',', skiprows=1, dtype=str)[:,2:].astype(np.float32).mean(0)
+    for c in range(1, N_CLASSES):
+        print_and_log('%s Macro Acc: %.2f%%' % (CLASS_NAMES[c], macro_acc_test[c-1]), log_file)
+
+    with open(fold_wise_acc_file, 'a') as f:
+        f.write('%s,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n' % (fold, val_acc, class_acc[0], class_acc_top2[0], macro_acc_test[0],
+            class_acc[1], class_acc_top2[1], macro_acc_test[1], class_acc[2], class_acc_top2[2], macro_acc_test[2]))
