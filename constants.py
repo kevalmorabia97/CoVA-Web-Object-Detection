42a1a63f44137b6f52f8f65b4a696241ea698811
Keval Morabia <kevalmorabia97@gmail.com>
2020-11-04 23:40:33 -0600

    add comments to constants.py

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 class Constants:
-    SEED = 123
-    DATA_DIR = '../data/' # Contains imgs/*.png, bboxes/*.pkl, and additional_features/*.pkl (optional) files
-    SPLIT_DIR = 'splits'
-    CLASS_NAMES = ['BG', 'Price', 'Title', 'Image']
+    SEED = 123 # in hope of achieving reproducible results
+    DATA_DIR = '../data/' # contains imgs/*.png, bboxes/*.pkl, and additional_features/*.pkl (optional)
+    SPLIT_DIR = 'splits' # contains Fold-%d dir containing {train|val|test}_{imgs|domains}.txt and webpage_info.csv (optional)
+    CLASS_NAMES = ['BG', 'Price', 'Title', 'Image'] # Accuracies of class-0 (BG) are ignored
     N_CLASSES = len(CLASS_NAMES)
-    IMG_HEIGHT = 1280 # Image assumed to have same height and width
+    IMG_HEIGHT = 1280 # image assumed to have same height and width
     OUTPUT_DIR = 'results_5-Fold_CV' # results dir is created here
