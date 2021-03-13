4410bef972f3267cbdc5a31d24355c0bb3326b9d
Keval Morabia <kevalmorabia97@gmail.com>
2021-03-13 06:00:36 +0000

    convert datasets from pkl to csv

--- datasets.py
+++ datasets.py
@@ -4,8 +4,6 @@ import torch
 from torch.utils.data import DataLoader
 import torchvision
 
-from utils import pkl_load
-
 
 class WebDataset(torchvision.datasets.VisionDataset):
     """
@@ -15,15 +13,15 @@ class WebDataset(torchvision.datasets.VisionDataset):
         """
         Args:
             root: directory where data is located
-                Must contain imgs/x.png Image and corresponding bboxes/x.pkl Bounding Boxes files
+                Must contain imgs/*.png Image and corresponding bboxes/*.csv Bounding Boxes files
                 BBox file should have bboxes in pre-order and each row corresponds to [x,y,w,h,label]
             img_ids: list of img_names to consider
             context_size: number of BBoxes before and after to consider as context / graph neighborhood (int)
                 If set to 0, `context_indices` will be empty as it will not be used
             use_additional_feats: whether to use additional features (default: False)
-                if True, `root` directory must contain additional_features/x.pkl additional features which is a numpy array of shape [n_bboxes, n_additional_feat]
+                if True, `root` directory must contain additional_features/*.csv
             sampling_fraction: randomly sample this many (float between 0 and 1) fraction of background boxes (class 0) while training (default: 1 --> no sampling, take all)
-                All samples of class > 0 are always taken
+                All samples of class > 0 are always taken, relative ordering of bboxes is preserved
                 NOTE: For val and test data, sampling_fraction SHOULD be 1 (no sampling)
         """
         super(WebDataset, self).__init__(root)
@@ -39,14 +37,19 @@ class WebDataset(torchvision.datasets.VisionDataset):
         self.sampling_fraction = sampling_fraction
 
         self.imgs_paths = ['%s/imgs/%s.png' % (self.root, img_id) for img_id in self.ids]
-        self.all_bboxes = [pkl_load('%s/bboxes/%s.pkl' % (self.root, img_id)) for img_id in self.ids]
+        self.all_bboxes = [
+            np.loadtxt('%s/bboxes/%s.csv' % (self.root, img_id), delimiter=',', skiprows=1, dtype='float32')
+            for img_id in self.ids
+        ]
         
         if use_additional_feats:
-            self.all_additional_tensor_features = [torch.Tensor(pkl_load('%s/additional_features/%s.pkl' % (self.root, img_id))) for img_id in self.ids]
-            self.n_additional_feat = len(self.all_additional_tensor_features[0][0])
+            self.all_additional_tensor_features = [
+                torch.Tensor(np.loadtxt('%s/additional_features/%s.csv' % (self.root, img_id), delimiter=',', skiprows=1, dtype='float32')) 
+                for img_id in self.ids
+            ]
         else:
             self.all_additional_tensor_features = [torch.empty(len(bboxes), 0) for bboxes in self.all_bboxes]
-            self.n_additional_feat = 0
+        self.n_additional_feat = self.all_additional_tensor_features[0].shape[1]
     
     def __getitem__(self, index):
         """
@@ -104,26 +107,25 @@ def custom_collate_fn(batch):
     custom collate_fn has to be created that creates a batch
     
     Args:
-        batch: list of N=`batch_size` tuples. Example [(img_id_1, img_1, bboxes_1, afs_1, ci_1, labels_1), ..., (img_id_N, img_N, bboxes_N, afs_N, ci_N, labels_N)]
+        batch: list of (img_id, img, bboxes, afs, ci, labels) for all elements in batch
     
     Returns:
-        batch: contains img_ids, images, bboxes, context_indices, labels
-            img_ids: names of images (string) to compute imgwise (webpagewise) and domainwise (macro) Accuracies
-            images: torch.Tensor [N, 3, img_H, img_W]
-            bboxes: torch.Tensor [total_n_bboxes_in_batch, 5]
-                each each of [batch_img_index, top_left_x, top_left_y, bottom_right_x, bottom_right_y]
-            additional_feats: torch.Tensor of size [total_n_bboxes_in_batch, n_additional_feat]
-            context_indices: torch.LongTensor [total_n_bboxes_in_batch, 2*context_size] i.e. bbox indices (0-indexed) of contexts for all n bboxes.
-                If not enough found, rest are -1
-            labels: torch.LongTensor [total_n_bboxes_in_batch]
+        img_ids: names of images (string) to compute imgwise (webpagewise) and domainwise (macro) Accuracies
+        images: torch.Tensor of size [batch_size, 3, img_H, img_H]
+        bboxes: torch.Tensor [N, 5], N = total_n_bboxes_in_batch
+            each of [batch_img_index, top_left_x, top_left_y, bottom_right_x, bottom_right_y]
+        additional_feats: torch.Tensor [N, n_additional_feat]
+        context_indices: Torch.LongTensor [N, n_context]
+            indices (0 to N-1) of `n_context` bboxes that are in context for a given bbox. If not enough found, rest are -1
+        labels: torch.LongTensor [N]
     """
     img_ids, images, bboxes, additional_feats, context_indices, labels = zip(*batch)
-    # img_ids = (img_id_1, ..., img_id_N)
-    # images = (img_1, ..., img_N) each element of size [3, img_H, img_W]
-    # bboxes = (bboxes_1, ..., bboxes_N) each element of size [n_bboxes_in_image, 4]
-    # additional_feats = (additional_feats_1, ..., additional_feats_N) each element of size [n_bboxes_in_image, n_additional_feat]
-    # context_indices = (ci_1, ..., ci_N) each element of size [n_bboxes_in_image, 2*context_size]
-    # labels = (labels_1, ..., labels_N) each element of size [n_bboxes_in_image]
+    # img_ids = (img_id_1, img_id_2, ...)
+    # images = (img_1, img_2, ...) each element of size [3, img_H, img_W]
+    # bboxes = (bboxes_1, bboxes_2, ...) each element of size [n_bboxes_in_image, 4]
+    # additional_feats = (additional_feats_1, additional_feats_2, ...) each element of size [n_bboxes_in_image, n_additional_feat]
+    # context_indices = (ci_1, ci_2, ...) each element of size [n_bboxes_in_image, 2*context_size]
+    # labels = (labels_1, labels_2, ...) each element of size [n_bboxes_in_image]
     
     img_ids = np.array(img_ids)
     images = torch.stack(images, 0)
@@ -147,16 +149,16 @@ def custom_collate_fn(batch):
 def load_data(data_dir, train_img_ids, val_img_ids, test_img_ids, context_size, batch_size, use_additional_feats=False, sampling_fraction=1, num_workers=4):
     """
     Args:
-        data_dir: directory which contains imgs/x.png Image and corresponding bboxes/x.pkl BBox coordinates file
+        data_dir: directory which contains imgs/*.png Image and corresponding bboxes/*.csv BBox coordinates file
         train_img_ids: list of img_names to consider in train split
         val_img_ids: list of img_names to consider in val split
         test_img_ids: list of img_names to consider in test split
         context_size: number of BBoxes before and after to consider as context
         batch_size: size of batch in train_loader
         use_additional_feats: whether to use additional features (default: False)
-            if True, `root` directory must contain additional_feats/x.pkl additional features which is a numpy array of shape [n_bboxes, n_additional_feat]
+            if True, `root` directory must contain additional_feats/*.csv
         sampling_fraction: randomly sample this many fraction of background boxes (class 0) while training (default: 1 --> no sampling, take all)
-            All samples of class > 0 are always taken
+            All samples of class > 0 are always taken, relative ordering of bboxes is preserved
     
     Returns:
         train_loader, val_loader, test_loader (torch.utils.data.DataLoader)
